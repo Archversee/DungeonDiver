2a93058cf048d8d80057445b3629c5baadb4b11b
Archversee <54101952+Archversee@users.noreply.github.com>
2021-05-12 09:10:17 +0800

    MovementDash

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -5,63 +5,106 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+
+    [SerializeField]
+    private LayerMask dashLayerMask;
+
     public float speed;
     public float idletime;
     private Vector2 dir;
+    private Vector3 movedir;
     private Animator animator;
+    private Rigidbody2D rigidbody2D;
     public GameController gameController;
 
-    private void Start()
+    public float dashrange;
+    public float dashcooldown;
+
+    bool isDashButtonDown = false;
+
+    private void Awake()
     {
         animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
         TakeInput();
-        Move();
         idletime += Time.deltaTime;
-    }
-
-    private void Move()
-    {
-        dir.Normalize();
-        dir *= speed;
-        transform.Translate(dir * Time.deltaTime);
-
-        if(dir.x != 0 || dir.y != 0)
-        {
-            SetAnimatorMovement(dir, idletime); //change animator if moving
-        }
-        else if(idletime >= 0.01)
-        {
-            SetAnimatorMovement(dir, idletime); //change to idle state
-        }
     }
 
     private void TakeInput()
     {
         dir = Vector2.zero;  //reset dir to default every frame
 
-        if(Input.GetKey(KeyCode.W))
+        float moveX = 0f;
+        float moveY = 0f;
+
+        if (Input.GetKey(KeyCode.W))
         {
             dir += Vector2.up;
+            moveY = +1f;
             idletime = 0.0f;
         }
         if (Input.GetKey(KeyCode.S))
         {
             dir += Vector2.down;
+            moveY = -1f;
             idletime = 0.0f;
         }
         if (Input.GetKey(KeyCode.A))
         {
             dir += Vector2.left;
+            moveX = -1f;
             idletime = 0.0f;
         }
         if (Input.GetKey(KeyCode.D))
         {
             dir += Vector2.right;
+            moveX = +1f;
             idletime = 0.0f;
+        }
+
+        movedir = new Vector3(moveX, moveY).normalized;
+
+        if (dir.x != 0 || dir.y != 0)
+        {
+            SetAnimatorMovement(dir, idletime); //change animator if moving
+        }
+        else if (idletime >= 0.01)
+        {
+            SetAnimatorMovement(dir, idletime); //change to idle state
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (dashcooldown < 0f)
+            {
+                isDashButtonDown = true;
+                dashcooldown = 7f;
+            }
+        }
+    }   
+
+    private void FixedUpdate()
+    {
+        dashcooldown -= 0.1f;
+        rigidbody2D.velocity = movedir * speed;
+
+        if(isDashButtonDown)
+        {
+            float dashAmt = 1.5f;
+            Vector3 dashpos = transform.position + movedir * dashAmt;
+
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.position, movedir, dashAmt, dashLayerMask);
+            if(raycastHit2D.collider != null)
+            {
+                dashpos = raycastHit2D.point;
+            }
+
+            rigidbody2D.MovePosition(dashpos);
+            isDashButtonDown = false;
         }
     }
 
